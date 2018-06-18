a8f96c71c3ef02dc994999ccdca639b016df0c40
Aravindh Babu <aravindhrj55@gmail.com>
2018-06-18 16:22:39 +0530

    added screengrab just beginning stage

--- mypro01.py
+++ mypro01.py
@@ -1,6 +1,11 @@
 #Importing
 
 from tkinter import *
+#from PIL import ImageGrab
+import time
+import autopy
+
+
 
 import tkinter.filedialog
 
@@ -18,6 +23,16 @@ def files():
     file = tkinter.filedialog.askopenfilename()
     show.set(file)
 show = StringVar(None)
+
+def hell():
+    time.sleep(5)
+    ImageGrab.grab().save("Screencap.jpg","JPEG")
+
+
+def hoii():
+    #time.sleep()
+    pic=autopy.bitmap.capture_screen()
+    pic.save("/root/Downloads/m.png")
 
 
 
@@ -38,6 +53,7 @@ def hiithere():
 
 
 def hiither():
+    kill(hiithere)
     inp1=Entry(riteframe)
     inp1.config(text="source")
     inp1.grid(row=2,column=2,sticky=S)
@@ -60,13 +76,15 @@ riteframe.pack(side=RIGHT)
 but1 = Button(leftframe,width=20,height=2, command=hiithere)
 but1.config(text="FileTransfer")
 but1.grid(row=0,sticky=S)
-but1= Button(leftframe,command=hiithere)
+
 but1= Button(leftframe,width=20,height=2,command=hiither)
 but1.config(text="FTP ")
 but1.grid(row=1,sticky=N)
-but1= Button(leftframe,command=quit)
-but1.config(text="scrren grab")
+
+but1= Button(leftframe,command=hoii)
+but1.config(text="scrren grab1")
 but1.grid(row=2,sticky=N)
+
 but1= Button(leftframe,command=quit)
 but1.config(text="scrren grab")
 but1.grid(row=4,sticky=N)
